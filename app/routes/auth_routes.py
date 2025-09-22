d97e6a0f70d9bd1d9bab02f8b2b93c6fd6dd3368
Amulya21666 <amulyabm21@gmail.com>
2025-09-22 10:16:46 +0530

    Update auth_routes.py

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Request, Form, Depends, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 
 from app.database import get_db
 from app import crud
